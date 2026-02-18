de03da0d4d1bbc4c30f5bef25707e76161b8b95a
Yash Patel <yp969803@gmail.com>
2026-02-19 00:05:44 +0530

    fix: remove unnecessary blank lines in licensing comments across multiple files

--- rag_control/exceptions/base.py
+++ rag_control/exceptions/base.py
@@ -3,5 +3,6 @@ Copyright (c) 2026 RetrievalLabs Co. All rights reserved.
 Licensed under the RetrievalLabs Business-Restricted License (RBRL) v1.0.
 """
 
+
 class RagControlError(Exception):
     """Base exception for rag_control."""
--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -2,5 +2,3 @@
 Copyright (c) 2026 RetrievalLabs Co. All rights reserved.
 Licensed under the RetrievalLabs Business-Restricted License (RBRL) v1.0.
 """
-
-
