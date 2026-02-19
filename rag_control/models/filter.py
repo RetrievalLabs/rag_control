8dc391e3f969d40b881972364c0e61486915eed2
Yash Patel <yp969803@gmail.com>
2026-02-19 17:26:22 +0530

    feat: enhance Filter and Condition models with new fields and operators

--- rag_control/models/filter.py
+++ rag_control/models/filter.py
@@ -1,7 +1,27 @@
-from pydantic import BaseModel
-from typing import  List
+from pydantic import BaseModel, Field
+
+from typing import Literal, Optional, List
+
+Operator = Literal[
+    "equals",
+    "in",
+    "intersects",
+    "lte",
+    "gte"
+]
+
+class Condition(BaseModel):
+    field: str
+    operator: Operator
+    source: Literal["context"] = "context"
 
 
 class Filter(BaseModel):
+    and_: Optional[List["Filter"]] = Field(default=None, alias="and")
+    or_: Optional[List["Filter"]] = Field(default=None, alias="or")
+    condition: Optional[Condition] = None
+
+    class Config:
+        allow_population_by_field_name = True
     
 
