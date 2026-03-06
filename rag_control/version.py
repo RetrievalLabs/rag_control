56634b8caaff60fb90f5463b5a279d2cc283b05d
Yash Patel <yp969803@gmail.com>
2026-03-06 20:03:49 +0530

    chore: update version to 0.1.1 in documentation and codebase

--- rag_control/version.py
+++ rag_control/version.py
@@ -3,4 +3,4 @@ Copyright (c) 2026 RetrievalLabs Co. All rights reserved.
 Licensed under the RetrievalLabs Business-Restricted License (RBRL) v1.0.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
