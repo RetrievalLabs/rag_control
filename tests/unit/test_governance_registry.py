e07a46cd3bcdd64009f54bae7cf4f516e8651b9a
Yash Patel <yp969803@gmail.com>
2026-03-13 13:31:05 +0530

    refactor: update import statement in test_governance_registry.py to include Any type

--- tests/unit/test_governance_registry.py
+++ tests/unit/test_governance_registry.py
@@ -3,7 +3,7 @@ Copyright (c) 2026 RetrievalLabs Co. All rights reserved.
 Licensed under the RetrievalLabs Business-Restricted License (RBRL) v1.0.
 """
 
-from typing import TypedDict
+from typing import Any, TypedDict
 
 import pytest
 
